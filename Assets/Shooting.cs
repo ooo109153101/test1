5478471c59dbec3019841c389e34ea1d1333c3d3
ooo109153101 <3757217@gmail.com>
2023-05-11 11:43:33 +0800

    可以射擊
    新增攻擊點(空物件)，把攻擊點用成槍的子物件，移到槍口處->改程式碼後，把攝影機、攻擊點、預置物各丟到shooting的public->把Cube的Tag改成Target，為它加rigidbody

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     [Header("參考物件")]
-    public Camera PlayerCamera;
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    [Header("子彈預置物件")]
+    public GameObject bullet;
 
     private void Update()
     {
-        // 判斷有沒有按下左鍵
-        if (Input.GetMouseButtonDown(0) == true)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));  // 從攝影機射出一條射線
-            RaycastHit hit;  // 宣告一個射擊點
-            Vector3 targetPoint;  // 宣告一個位置點變數，到時候如果有打到東西，就存到這個變數
+            Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view           
 
-            // 如果射線有打到具備碰撞體的物件
-            if (Physics.Raycast(ray, out hit) == true)
-                targetPoint = hit.point;         // 將打到物件的位置點存進 targetPoint
+            RaycastHit hit;
+
+            Vector3 targetPoint;
+            if (Physics.Raycast(ray, out hit))
+                targetPoint = hit.point;
             else
-                targetPoint = ray.GetPoint(75);  // 如果沒有打到物件，就以長度75的末端點取得一個點，存進 targetPoint
+                targetPoint = ray.GetPoint(75); //Just a point far away from the player
 
-            Debug.DrawRay(ray.origin, targetPoint - ray.origin, Color.red, 10); // 畫出這條射線
+            Debug.DrawRay(ray.origin, targetPoint - ray.origin, Color.red, 10); // 在測試階段將射線設定為紅色線條，來看看線條長度夠不夠？
+
+            //Calculate direction from attackPoint to targetPoint
+            Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+            GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
+            currentBullet.transform.forward = directionWithoutSpread.normalized;
+
+            currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * 20, ForceMode.Impulse);//Impulse:用的力道是一次性的，所以會曲線性墜落(也有連續的)
         }
     }
 }
